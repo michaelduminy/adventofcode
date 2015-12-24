4aa7cd37cd249529857ff68836e247853d714c35
Mike Duminy <mike@22seven.com>
2015-12-24 13:32:24 +0200

    day16 both parts

---
+++ day16/app.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const fs = require('fs');
+const _ = require('lodash');
+
+let getFileContents = path => fs.readFileSync(path, 'utf8').trim();
+
+let lines = _(getFileContents('./input.txt').split('\n'))
+	.map(n => n.replace(/[:|,]/g, '').split(' '))
+	.map(n => {
+		let r = {};
+		for(let i = 0; i < n.length; i += 2){
+			r[n[i]] = parseInt(n[i+1])
+		}
+		return r;
+	}).value();
+console.log(lines)
+
+let match = {};
+_(getFileContents('./output.txt').split('\n'))
+	.map(n => n.replace(':', '').split(' '))
+	.map(n => {
+		match[n[0]] = parseInt(n[1])
+	}).value();
+
+console.log(match)
+
+let matches = [];
+for (let i = 0; i < lines.length; i++){
+	let is_match = [];
+	for (let p in lines[i]){
+		if (p == 'Sue') continue;
+		// console.log(p, lines[i][p], match[p])
+
+		if(lines[i][p] == match[p]) {
+			is_match.push(true);
+		} else {
+			is_match.push(false);
+		}
+	}
+
+	console.log(is_match)
+
+	if (_.every(is_match, Boolean)) matches.push(lines[i])
+}
+
+console.log(matches)
